81332d3b612374695ff8d3b5f5d645aafeb37a65
Benjamin A <97291322+Baoufa@users.noreply.github.com>
2023-04-26 17:55:03 +0200

    feat: tokyo ui desktop only

---
+++ src/utils/getLargeIdentifier.ts
@@ -0,0 +1,9 @@
+export const getLargeIdentifier = (identifier: string): string => {
+  if (identifier) {
+    if (identifier.length <= 20) return identifier;
+
+    const last = identifier?.slice(-4);
+    const start = identifier?.slice(0, 10);
+    return start + "..." + last;
+  } else return "...";
+};
