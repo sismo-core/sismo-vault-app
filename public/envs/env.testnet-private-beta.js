ebd926562e75c6d79aca7ce9eb34dff5576f9e22
Dev <gabinhry@gmail.com>
2023-04-27 14:07:18 +0200

    fix: update env file

--- public/envs/env.testnet-private-beta.js
+++ public/envs/env.testnet-private-beta.js
@@ -2,7 +2,7 @@
 window.env = {
     name: "DEV_BETA",
     disabledSentry: false,
-    mintingAppUrl: "http://127.0.0.1:3001",
+    mintingAppUrl: "https://testnets.zikies.io",
     vaultV2URL: "https://vault-beta-api.zikies.io",
     vaultV1URL: "https://vault-api.zikies.io",
     hubApiUrl: "https://hub.testnets.sismo.io",
