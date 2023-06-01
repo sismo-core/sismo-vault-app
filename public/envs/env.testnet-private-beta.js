cfe79eb2aeb6bd443dd0a930c34594ba60316eed
leo Sayous <leo@sismo.io>
2023-06-01 18:52:39 +0200

    feat: change testnet private beta telegram botId

--- public/envs/env.testnet-private-beta.js
+++ public/envs/env.testnet-private-beta.js
@@ -1,4 +1,4 @@
-//app.sismo.io
+// vault-beta.zikies.io
 window.env = {
     name: "DEV_BETA",
     disabledSentry: false,
@@ -13,7 +13,7 @@ window.env = {
     commitmentMapperUrlV2: "https://p13gt0vnph.execute-api.eu-west-1.amazonaws.com",
     commitmentMapperUrlV1: "https://x5y521b36b.execute-api.eu-west-1.amazonaws.com",
     githubOauthClientId: "35b1e225b5f8f1401cc3",
-    telegramBotId:"6223780197",
+    telegramBotId:"5851173581",
     chainName: "goerli",
     sismoDestination: {
       address: "0x0000000000000000000000000000000000515110",
