92bef86ed7a4064d36095836eb846eb54c7819e2
Dev <gabinhry@gmail.com>
2023-06-02 12:21:11 +0200

    fix: update commitment mapper v2 url

--- public/envs/env.staging-beta.js
+++ public/envs/env.staging-beta.js
@@ -7,7 +7,7 @@ window.env = {
   vaultV1URL: "https://vault-api.zikies.io",
   hubApiUrl: "https://hub.staging.zikies.io",
   factoryApiUrl: "https://factory-api.staging.zikies.io",
-  commitmentMapperUrlV2: "https://x93oogcd5e.execute-api.eu-west-1.amazonaws.com",
+  commitmentMapperUrlV2: "https://httqvtidxi.execute-api.eu-west-1.amazonaws.com",
   commitmentMapperUrlV1: "https://sibgc1bwn8.execute-api.eu-west-1.amazonaws.com",
   githubOauthClientId: "ed165a166320d0676ccb",
   chainName: "goerli",
