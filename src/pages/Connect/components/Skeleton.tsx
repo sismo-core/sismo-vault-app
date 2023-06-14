383e68ed5198038689d256e5da0adcebec74520d
Benjamin A <97291322+Baoufa@users.noreply.github.com>
2023-06-14 14:49:41 +0200

    feat: add skeleton on loading (#55)

--- src/pages/Connect/components/Skeleton.tsx
+++ src/pages/Connect/components/Skeleton.tsx
@@ -1,15 +1,10 @@
 import styled, { keyframes } from "styled-components";
+import { SismoConnectRequest } from "../../../libs/sismo-connect-provers";
+import { CaretUp, CheckCircle } from "phosphor-react";
+import colors from "../../../theme/colors";
+import Toggle from "../Flow/components/Toggle";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: 366px;
-  //box-sizing: border-box;
-  padding: 40px 24px;
-`;
-
-export const SkeletonLoading = keyframes`
+export const SkeletonKeyframes = keyframes`
   from {
     background-position-x: 0%;
   }
@@ -18,103 +13,292 @@ export const SkeletonLoading = keyframes`
   }
 `;
 
-const Separator = styled.div`
-  width: 252px;
-  height: 1px;
-  background: ${(props) => props.theme.colors.blue9};
-  margin: 10px 0;
-  box-sizing: border-box;
-`;
-
-const Wrapper = styled.div`
-  justify-content: center;
-
+const SkeletonColor = styled.div`
   background: linear-gradient(
     90deg,
     rgba(42, 53, 87, 0.3) 5%,
     rgba(42, 53, 87, 0.6) 25%,
     rgba(42, 53, 87, 0.3) 50%
   );
-  clip-path: url(#svgClipPathID);
   background-size: 200% 100%;
-  animation: ${SkeletonLoading};
+  animation: ${SkeletonKeyframes};
   animation-duration: 1.5s;
   animation-timing-function: linear;
   animation-iteration-count: infinite;
   box-sizing: border-box;
 `;
 
-const TopWrapper = styled(Wrapper)`
-  clip-path: url(#svgClipPathID1);
-`;
-const MiddleWrapper = styled(Wrapper)`
-  clip-path: url(#svgClipPathID2);
-`;
-const BottomWrapper = styled(Wrapper)`
-  clip-path: url(#svgClipPathID3);
-  transform: translateY(46px);
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  padding: 40px 24px;
+  background: ${(props) => props.theme.colors.blue11};
+  border-radius: 10px;
+  z-index: 2;
+  box-sizing: border-box;
 `;
 
-export default function Skeleton() {
+const Title = styled(SkeletonColor)`
+  height: 20px;
+  width: 300px;
+  border-radius: 5px;
+`;
+
+const ContentTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 24px;
+`;
+
+const AppLogo = styled(SkeletonColor)`
+  width: 72px;
+  height: 72px;
+  border-radius: 50%;
+  margin-bottom: 12px;
+`;
+
+const SecondLine = styled.div`
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  font-family: ${(props) => props.theme.fonts.semibold};
+  color: ${(props) => props.theme.colors.blue0};
+  font-size: 20px;
+  line-height: 24px;
+`;
+
+const SkeletonTitle = styled(SkeletonColor)`
+  height: 24px;
+  width: 300px;
+  border-radius: 5px;
+`;
+
+const CallToAction = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 252px;
+  align-self: center;
+  gap: 10px;
+`;
+
+const LinkWrapper = styled(SkeletonColor)`
+  align-self: flex-end;
+  width: 148px;
+  height: 18px;
+  border-radius: 5px;
+`;
+
+const DataRequests = styled.div`
+  border: 1px solid ${(props) => props.theme.colors.blue7};
+  border-radius: 6px;
+  margin-bottom: 24px;
+`;
+const RequiredWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 16px;
+`;
+
+const OptListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 0px;
+`;
+
+const Line = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 16px 0px;
+`;
+
+const Request = styled(SkeletonColor)<{ width: number }>`
+  width: ${(props) => props.width}px;
+  height: 24px;
+  border-radius: 5px;
+`;
+
+const CheckCircleIcon = styled(CheckCircle)`
+  flex-shrink: 0;
+`;
+
+const OptionalSeparator = styled.div`
+  height: 1px;
+  width: 100%;
+  background: ${(props) => props.theme.colors.blue7};
+`;
+
+const ItemSeparator = styled.div`
+  height: 1px;
+  background: ${(props) => props.theme.colors.blue9};
+`;
+
+const OptionalWrapper = styled.div`
+  padding: 0px 16px;
+`;
+
+const OptionalList = styled.div<{ isFolded: boolean }>`
+  display: flex;
+  flex-direction: column;
+  visibility: ${(props) => (props.isFolded ? "hidden" : "visible")};
+  height: ${(props) => (props.isFolded ? "0px" : "auto")};
+`;
+
+const OptionalTitle = styled.div<{ folded: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  line-height: 20px;
+  font-family: ${(props) => props.theme.fonts.medium};
+  color: ${(props) => props.theme.colors.blue3};
+  padding-top: 16px;
+  padding-bottom: ${(props) => (!props.folded ? "16px" : "0px")};
+`;
+
+const CaretWrapper = styled.div<{ folded: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform: ${(props) => (props.folded ? "rotateX(0deg)" : "rotateX(180deg)")};
+  cursor: pointer;
+`;
+
+const ButtonSkeleton = styled(SkeletonColor)`
+  width: 252px;
+  height: 40px;
+  border-radius: 5px;
+`;
+
+const ImpersonatedBanner = styled(SkeletonColor)`
+  width: 100%;
+  height: 81px;
+  padding: 12px 16px;
+  border-radius: 5px;
+  box-sizing: border-box;
+  margin-bottom: 24px;
+`;
+
+const SignatureWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin-bottom: 24px;
+`;
+
+const SignatureTitle = styled(SkeletonColor)`
+  width: 200px;
+  height: 20px;
+  border-radius: 5px;
+`;
+const TextArea = styled(SkeletonColor)`
+  border-radius: 5px;
+  height: 78px;
+`;
+
+type Props = {
+  sismoConnectRequest: SismoConnectRequest;
+};
+
+export default function Skeleton({ sismoConnectRequest }: Props) {
+  const requiredRequests = [];
+  const optionalRequests = [];
+  const isImpersonated = sismoConnectRequest?.vault?.impersonate?.length > 0;
+  const isSignature = sismoConnectRequest?.signature?.message?.length > 0;
+
+  for (const claim of sismoConnectRequest?.claims) {
+    if (!claim?.isOptional) {
+      requiredRequests.push(claim);
+    } else {
+      optionalRequests.push(claim);
+    }
+  }
+
+  for (const auth of sismoConnectRequest?.auths) {
+    if (!auth?.isOptional) {
+      requiredRequests.push(auth);
+    } else {
+      optionalRequests.push(auth);
+    }
+  }
+
   return (
     <Container>
-      <TopWrapper>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 200 178"
-          enableBackground="new 0 0 200 178"
-          xmlSpace="preserve"
-          width={200}
-          height={178}
-        >
-          <clipPath id="svgClipPathID1">
-            <path d="M97,0h6c24.9,0,45,20.1,45,45v6c0,24.9-20.1,45-45,45h-6c-24.9,0-45-20.1-45-45v-6C52,20.1,72.1,0,97,0z" />
-            <path d="M5,116h190c2.8,0,5,2.2,5,5v12c0,2.8-2.2,5-5,5H5c-2.8,0-5-2.2-5-5v-12C0,118.2,2.2,116,5,116z" />
-            <path d="M52,158h96c2.8,0,5,2.2,5,5v10c0,2.8-2.2,5-5,5H52c-2.8,0-5-2.2-5-5v-10C47,160.2,49.2,158,52,158z" />
-          </clipPath>
-        </svg>
-      </TopWrapper>
-      <Separator />
-      <MiddleWrapper>
-        <svg
-          version="1.1"
-          id="Calque_1"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 120 24"
-          enableBackground="new 0 0 120 24"
-          xmlSpace="preserve"
-          width={120}
-          height={24}
-        >
-          <clipPath id="svgClipPathID2">
-            <path d="M5,0h110c2.8,0,5,2.2,5,5v14c0,2.8-2.2,5-5,5H5c-2.8,0-5-2.2-5-5V5C0,2.2,2.2,0,5,0z" />
-          </clipPath>
-        </svg>
-      </MiddleWrapper>
-      <Separator />
-      <BottomWrapper>
-        <svg
-          version="1.1"
-          id="Calque_1"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 252 68"
-          enableBackground="new 0 0 252 68"
-          xmlSpace="preserve"
-          width={252}
-          height={68}
-        >
-          <clipPath id="svgClipPathID3">
-            <path d="M117,0h130c2.8,0,5,2.2,5,5v8c0,2.8-2.2,5-5,5H117c-2.8,0-5-2.2-5-5V5C112,2.2,114.2,0,117,0z" />
-            <path d="M5,28h242c2.8,0,5,2.2,5,5v30c0,2.8-2.2,5-5,5H5c-2.8,0-5-2.2-5-5V33C0,30.2,2.2,28,5,28z" />
-          </clipPath>
-        </svg>
-      </BottomWrapper>
+      <ContentTitle>
+        <AppLogo />
+        <SecondLine>
+          <SkeletonTitle />
+        </SecondLine>
+      </ContentTitle>
+
+      {isImpersonated && <ImpersonatedBanner />}
+
+      <Title style={{ marginBottom: 8 }} />
+
+      <DataRequests>
+        {requiredRequests.map((request, index) => (
+          <RequiredWrapper key={index + "/required"}>
+            {" "}
+            {index !== 0 && <ItemSeparator />}
+            <Line>
+              <CheckCircleIcon size={24} color={colors.blue6} />
+              <Request width={320} />
+            </Line>
+          </RequiredWrapper>
+        ))}
+
+        {optionalRequests?.length > 0 && (
+          <>
+            {" "}
+            <OptionalSeparator />
+            <OptionalWrapper>
+              <OptionalTitle folded={true} onClick={() => {}}>
+                <div>Optional</div>
+                <CaretWrapper folded={true}>
+                  <CaretUp size={16} color={colors.blue3} />
+                </CaretWrapper>
+              </OptionalTitle>
+
+              {optionalRequests.map((request, index) => (
+                <OptListWrapper key={index + "/optional"}>
+                  {" "}
+                  {index !== 0 && <ItemSeparator />}
+                  <Line key={index}>
+                    <Toggle
+                      isDisabled={true}
+                      value={false}
+                      onChange={() => {}}
+                    />
+                    <Request width={304} />
+                  </Line>
+                </OptListWrapper>
+              ))}
+
+              <OptionalList isFolded={true}></OptionalList>
+            </OptionalWrapper>{" "}
+          </>
+        )}
+      </DataRequests>
+
+      {isSignature && (
+        <SignatureWrapper>
+          <SignatureTitle />
+          <TextArea />
+        </SignatureWrapper>
+      )}
+
+      <CallToAction>
+        <LinkWrapper />
+        <ButtonSkeleton />
+      </CallToAction>
     </Container>
   );
 }
